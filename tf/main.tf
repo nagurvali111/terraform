2d558e260525f023cf82dd7c1037d4e32365b04a
nagurvali111 <nagurvali111@gmail.com>
2024-12-03 15:30:20 +0530

    Update main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -1,3 +1,13 @@
+# Step 1: Configure the Terraform backend with S3
+terraform {
+  backend "s3" {
+    bucket  = "nagur112233"
+    key     = "terraform.tfstate"
+    region  = "ap-south-1"
+    encrypt = true
+  }
+}
+
 provider "aws" {
   region = "ap-south-1"
 }
