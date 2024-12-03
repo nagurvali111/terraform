a7fe24c434664676e0af9f5e53662fcd81dbc2b7
nagurvali111 <nagurvali111@gmail.com>
2024-12-03 15:47:11 +0530

    1

--- tf/main.tf
+++ tf/main.tf
@@ -11,11 +11,3 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_s3_bucket" "Nagur_tst" {
-  bucket = "nagur112233"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
